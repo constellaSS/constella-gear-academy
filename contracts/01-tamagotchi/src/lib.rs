d0ca07e5eb2b88a27f638c39f9f6b23d482da822
jonar12 <jona.arredondo12@gmail.com>
2024-01-30 16:02:27 -0600

    TODO 6 completed

--- contracts/01-tamagotchi/src/lib.rs
+++ contracts/01-tamagotchi/src/lib.rs
@@ -2,7 +2,7 @@
 
 #[allow(unused_imports)]
 use gstd::{exec, msg, prelude::*};
-use tamagotchi_io::Tamagotchi;
+use tamagotchi_io::{Tamagotchi, TmgAction, TmgEvent};
 
 static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 
@@ -22,6 +22,24 @@ extern fn init() {
 #[no_mangle]
 extern fn handle() {
     // TODO: 6️⃣ Add handling of `Name` and `Age` actions
+    let input_msg = msg::load().expect("Error in loading Tmg Input Message");
+    let tmg = unsafe { TAMAGOTCHI.as_mut().expect("The contract is not initialized") };
+    match input_msg {
+        TmgAction::Name => {
+            msg::reply(
+                TmgEvent::Name(tmg.name.clone()),
+                0,
+            )
+            .expect("Name not loaded correctly");
+        }
+        TmgAction::Age => {
+            msg::reply(
+                TmgEvent::Age(exec::block_timestamp() - tmg.date_of_birth),
+                0,
+            )
+            .expect("Age not loaded correctly");
+        }
+    }
 }
 
 #[no_mangle]
