f9ce25dd99a3b71b21117bc08c81aacd414fb1c7
jonar12 <jona.arredondo12@gmail.com>
2024-01-30 15:54:57 -0600

    TODO 5: attempt to fix not passing CI

--- contracts/01-tamagotchi/src/lib.rs
+++ contracts/01-tamagotchi/src/lib.rs
@@ -10,12 +10,11 @@ static mut TAMAGOTCHI: Option<Tamagotchi> = None;
 #[no_mangle]
 extern fn init() {
     // TODO: 5️⃣ Initialize the Tamagotchi program
-    let name: String = msg::load()
-        .expect("Can't decode the init message");
+    let name: String = msg::load().expect("Can't decode the init message");
 
     let tamagotchi = Tamagotchi {
         name,
-        date_of_birth: exec::block_timestamp()
+        date_of_birth: exec::block_timestamp(),
     };
 
     unsafe {
