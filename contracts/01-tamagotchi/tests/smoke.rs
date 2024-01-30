723888e738c04287611d95241371d09d6a2838c7
jonar12 <jona.arredondo12@gmail.com>
2024-01-30 15:06:15 -0600

    HW-01 completed

--- contracts/01-tamagotchi/tests/smoke.rs
+++ contracts/01-tamagotchi/tests/smoke.rs
@@ -1,4 +1,7 @@
-use gtest::{Program, System};
+use gtest::{Log, Program, System};
+use tamagotchi_io::{TmgAction, TmgEvent};
+
+const TEST_AGE: u64 = 30;
 
 #[test]
 fn smoke_test() {
@@ -7,4 +10,19 @@ fn smoke_test() {
     let _program = Program::current(&sys);
 
     // TODO: 8️⃣ Test the program initialization and message handling
+    let res = _program.send(2, String::from("Tamagotchi Name"));
+    assert!(!res.main_failed());
+
+    let res = _program.send(2, TmgAction::Name);
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Name(String::from("Tamagotchi Name")));
+    assert!(res.contains(&expected_log));
+
+    sys.spend_blocks(TEST_AGE as u32);
+    let res = _program.send(2, TmgAction::Age);
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Age(TEST_AGE * 1000));
+    assert!(res.contains(&expected_log));
 }
